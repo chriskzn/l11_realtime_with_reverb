0a1336058d4e2d1a8cc054c9fd084c27bf4254db
Christopher Wells <chrisw@wellswebtech.co.za>
2025-01-31 15:07:34 +0200

    Added Breeze package
    composer require laravel/breeze
    php artisan breeze:install

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,39 +1,15 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <div
-                        x-data="{
-                            messages: [],
-
-                            broadcastMessage () {
-                                fetch(`/broadcast`, { method: 'POST', headers: { 'X-CSRF-TOKEN': '{{ csrf_token() }}', 'Accept': 'application/json' } })
-                            }
-                        }"
-                        x-init="
-                            Echo.channel('chat')
-                                .listen('.Message', (e) => {
-                                    messages.push(e.body)
-                                })
-                        "
-                    >
-                        <div>
-                            <button x-on:click="broadcastMessage">Broadcast a message</button>
-
-                            <div class="mt-6" x-show="messages.length">
-                                <template x-for="message in messages">
-                                    <div x-text="message"></div>
-                                </template>
-                            </div>
-                        </div>
-                    </div>
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
